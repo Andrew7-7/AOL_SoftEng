967bb444f5c3fa72cfa4bfa76b9934f3112af0c3
Andrew7-7 <duduandrew7@gmail.com>
2024-02-23 13:08:57 +0700

    Add : Setup awal express js

---
+++ Backend/src/index.mjs
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import http from "http";
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const port = 3001;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log("hai berjalan di " + port);
+});
