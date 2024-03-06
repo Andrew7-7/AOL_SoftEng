979e78991c6b7e7299229af11f0486337d2e2774
Andrew <92193378+Andrew7-7@users.noreply.github.com>
2024-03-06 13:18:06 +0700

    Fix : Fix * CSS

--- Frontend/src/pages/login/view/login_page.tsx
+++ Frontend/src/pages/login/view/login_page.tsx
@@ -1,11 +1,40 @@
 import React from "react";
+import './login_css.css'
 import StudentNav from "../../../../public/components/navbar/student/student_navbar";
-
+import salyPic from "../../../../public/assets/Saly-38.png"
 const LoginPage = () => {
   return (
     <>
       <StudentNav />
-      <p>Login</p>
+      <div className="Outer">
+        <div className="divContainer">
+          <div className="leftDiv">
+            <p>Login</p>
+            <p>Welcome to StepCode</p>
+            <input
+              className="loginInput"
+              type="text"
+              placeholder="Email"
+            ></input>
+            <input
+              className="loginInput"
+              type="text"
+              placeholder="Password"
+            ></input>
+            <button>Sign In</button>
+          </div>
+          <div className="rightDiv">
+            <div className="innerRight">
+              <img src={salyPic}></img>
+              <p>Unleash Your Code Mastery</p>
+              <p>
+                Discover different kinds of programming language <br></br>with our
+                experts
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
