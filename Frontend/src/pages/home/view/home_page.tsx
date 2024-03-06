95cfadbcc5db5efdc6ddb130981850f46fb6e6ec
Andrew <92193378+Andrew7-7@users.noreply.github.com>
2024-03-06 11:57:27 +0700

    Add : Frontend Setup for pages (MVC)

---
+++ Frontend/src/pages/home/view/home_page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import StudentNav from "../../../../public/components/navbar/student/student_navbar";
+
+const HomePage = ()=>{
+
+    return (
+        <>
+        <StudentNav/>
+        
+        </>
+    )
+}
+
+export default HomePage;
---
+++ Frontend/src/pages/login/view/login_page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import StudentNav from "../../../../public/components/navbar/student/student_navbar";
+
+const LoginPage = () => {
+  return (
+    <>
+      <StudentNav />
+      <p>Login</p>
+    </>
+  );
+};
+
+export default LoginPage;
