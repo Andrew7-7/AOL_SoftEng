b54a2a3f083615fc526f616ed7942459bd92007c
hendrahartanto <hendrazhuo10@gmail.com>
2024-05-10 21:27:05 +0700

    fix

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -17,7 +17,6 @@ import AdminDummy from "./pages/admin/dummy/adminDummy";
 import IsBanned from "./pages/error/isBanned/isBanned";
 import ContactSidebar from "./pages/sidebar/contactSidebar";
 import ChatPage from "./pages/chat/chatPage";
-function App() {
 
 import PickTutorPage from "./pages/pickTutor/view/pick_tutor_page.tsx";
 function App() {
@@ -42,7 +41,7 @@ function App() {
             {/* Student Only Pages */}
             <Route element={<CheckRole role={"student"} />}>
               <Route path="/profile" element={<ProfilePage />}></Route>
-               <Route path="/:courseId/pickTutor" element={<PickTutorPage />} />
+              <Route path="/:courseId/pickTutor" element={<PickTutorPage />} />
             </Route>
 
             {/* Tutor Only Pages */}
