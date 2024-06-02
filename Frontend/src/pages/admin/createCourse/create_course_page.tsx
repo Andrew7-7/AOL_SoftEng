e8ed19272dc6ed4822f9f8f088ffe6f06b6d69fc
hendrahartanto <hendrazhuo10@gmail.com>
2024-06-02 15:08:11 +0700

    update

--- Frontend/src/pages/admin/createCourse/create_course_page.tsx
+++ Frontend/src/pages/admin/createCourse/create_course_page.tsx
@@ -2,17 +2,49 @@ import { ChangeEvent, useState } from "react";
 import AdminNav from "../../../global/components/navbar/admin/adminNav";
 import InputForm from "../../../global/components/textBox/InputForm";
 import "./create_course.css";
+import SelectForm from "../../../global/components/selectForm/SelectForm";
 
 const CreateCoursePage = () => {
 	const [courseFormData, setCourseFormData] = useState({
 		courseName: "",
 		courseDescription: "",
+		skill: "",
+		hourPerSession: "",
 	});
 
+	const skillLevelOption = [
+		{
+			id: "Beginer",
+			name: "Beginer",
+		},
+		{
+			id: "Intermediate",
+			name: "Intermediate",
+		},
+		{
+			id: "Advanced",
+			name: "Advanced",
+		},
+	];
+
 	const handleInputChange = (
-		event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+		event: ChangeEvent<
+			HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+		>
 	) => {
 		const { name, value } = event.target;
+
+		if (name === "hourPerSession") {
+			const num = parseFloat(value);
+			if (num < 1) {
+				setCourseFormData((prevData) => ({
+					...prevData,
+					[name]: "1", // Set the value to 1 if it is less than 1
+				}));
+				return;
+			}
+		}
+
 		setCourseFormData((prevData) => ({
 			...prevData,
 			[name]: value,
@@ -44,6 +76,21 @@ const CreateCoursePage = () => {
 										placeHolder="Insert course description"
 										type="textarea"
 										value={courseFormData.courseDescription}
+									/>
+									<SelectForm
+										label="Select skill level"
+										name="skill"
+										onChange={handleInputChange}
+										value={courseFormData.skill}
+										options={skillLevelOption}
+									/>
+									<InputForm
+										label="Hour per session"
+										name="hourPerSession"
+										onChange={handleInputChange}
+										placeHolder="Insert hour per session"
+										type="number"
+										value={courseFormData.hourPerSession}
 									/>
 								</div>
 								<div className="right"></div>
