6eb4dd48dde1fa0650b0528a32fd12f1d530b538
Andrew <92193378+Andrew7-7@users.noreply.github.com>
2024-06-11 11:20:55 +0700

    fix: fix UI

--- Frontend/src/pages/paymentpage/view/confirmed_page.tsx
+++ Frontend/src/pages/paymentpage/view/confirmed_page.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import StudentNav from '../../../global/components/navbar/student/student_navbar'
-import './confirmed_page.css'
+import React, { useEffect, useState } from "react";
+import StudentNav from "../../../global/components/navbar/student/student_navbar";
+import "./confirmed_page.css";
 
 const ConfirmedPage = () => {
   const [progress, setProgress] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
-      setProgress(prevProgress => {
+      setProgress((prevProgress) => {
         const newProgress = prevProgress + 50;
         return newProgress >= 100 ? 100 : newProgress;
       });
@@ -18,23 +18,28 @@ const ConfirmedPage = () => {
   return (
     <>
       <StudentNav />
-      <div className='paymentpage-banner'></div>
-      <div className='loading-bar-modal'>
-        <div className='loading-bar-modal-h1'>Processing your order ..</div>
-        <div className='loading-bar-modal-h3'>Please wait for 2 seconds </div>
-        <div className='loading-bar-progress-container'>
-          <div className='loading-bar-progress-modal-not-completed'>
-            <div className='loading-bar-progress-modal-completed'
-              style={{
-                width: `${progress}%`
-              }}
-            />
+      {/* <div className='paymentpage-banner'></div> */}
+      <div className="justifyDivPayment">
+        <div className="loading-bar-modal">
+          <div className="loading-bar-modal-h1">Processing your order ..</div>
+          <div className="loading-bar-modal-h3">Please wait for 2 seconds </div>
+          <div className="loading-bar-progress-container">
+            <div className="loading-bar-progress-modal-not-completed">
+              <div
+                className="loading-bar-progress-modal-completed"
+                style={{
+                  width: `${progress}%`,
+                }}
+              />
+            </div>
+          </div>
+          <div className="loading-bar-modal-h5">
+            After this you will be automatically directed to material page
           </div>
         </div>
-        <div className='loading-bar-modal-h5'>After this you will be automatically directed to material page</div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ConfirmedPage
+export default ConfirmedPage;
